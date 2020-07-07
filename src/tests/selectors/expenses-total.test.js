2b22ef2ce54122648fe8d8bade8bbd9d96a6fd93
John <hardyjohn142@hotmail.com>
2020-07-07 09:53:35 -0400

    add expense summary to dashboard

---
+++ src/tests/selectors/expenses-total.test.js
@@ -0,0 +1,14 @@
+import selectExpensesTotal from '../../selectors/expenses-total';
+import expenses from '../fixtures/expenses';
+
+test('should return 0 if no expenses', () => {
+    const res = selectExpensesTotal([]);
+
+    expect(res).toBe(0);
+});
+
+test('should correctly add up expenses total', () => {
+    const res = selectExpensesTotal([expenses[0]]);
+
+    expect(res).toBe(195);
+});
